396fe953542e9b43da12ce786ef8c9007ab16c82
Joao-Roberto13 <jringuane24@gmail.com>
2025-08-28 08:43:44 +0200

    2nd commit

--- LuNuts/settings.py
+++ LuNuts/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import certifi
+
+os.environ['SSL_CERT_FILE'] = certifi.where()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,4 +139,4 @@ EMAIL_HOST = 'smtp.googlemail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'farmaciakaya@gmail.com'
-EMAIL_HOST_PASSWORD = 'grzclbpnpsmdlrzp' #app password
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')  #app password
